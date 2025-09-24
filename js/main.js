6326738e734747afdcbe3fd6a072d2699c234c15
XenoKarma <andikapurnama1803@gmail.com>
2025-09-24 23:34:59 +0700

    day 4

--- js/main.js
+++ js/main.js
@@ -14,3 +14,27 @@ document.addEventListener('click', function(e){
     navbarNav.classList.remove('active');
   }
 })
+
+// Gallery
+const modal = document.getElementById("modal");
+    const modalImg = document.getElementById("modal-img");
+    const modalTitle = document.getElementById("modal-title");
+    const modalDesc = document.getElementById("modal-desc");
+
+    function openModal(imgSrc, title, desc) {
+      modal.style.display = "flex";
+      modalImg.src = imgSrc;
+      modalTitle.textContent = title;
+      modalDesc.textContent = desc;
+    }
+
+    function closeModal() {
+      modal.style.display = "none";
+    }
+
+    // Close modal if click outside content
+    window.onclick = function(e) {
+      if (e.target === modal) {
+        closeModal();
+      }
+    }
